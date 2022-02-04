6df9b8d2d1838a620d4686a0543f7061c49b1e15
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-02-03 21:25:02 -0700

    Fix typo: key should just be 'id'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      session[:user_id] = user.user_id
+      session[:user_id] = user.id
       redirect_to :root
     else
       redirect_to :new_user
