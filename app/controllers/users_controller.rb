49dcfcd12aba65d26e65e6849b8583b1b1771a21
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-02-08 21:17:53 +0000

    Call custom_save instance method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
-    if user.save
+    if user.custom_save
       session[:user_id] = user.id
       redirect_to :root
     else
