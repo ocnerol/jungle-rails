28c32aaa43dea9588ca21cc9447da140af2fe74f
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-02-08 04:12:09 +0000

    Make @category instance variable before running all validation specs

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,10 +1,14 @@
 require 'rails_helper'
+require 'pp'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
 
-    it 'successfully saves a product with all required fields present' do
+    before(:all) do
       @category = Category.create!(name: 'Electronics')
+    end
+
+    it 'successfully saves a product with all required fields present' do
       @product = Product.create!(name: 'iPhone', price_cents: 2000000, quantity: 4, category: @category)
       
       # checks to see if product newly created was successfully saved in database
@@ -12,22 +16,19 @@ RSpec.describe Product, type: :model do
     end
     
     it 'raises an error if name is not provided' do
-      @category = Category.create!(name: 'Electronics')
-      @product = Product.new(price_cents: 4000000, quantity: 3, category: @category)
+      @product = Product.create(price_cents: 4000000, quantity: 3, category: @category)
+      errors  = @product.errors.full_messages
       
-      # checls to see if there is an error message
-      expect(@product.valid?).to be_falsey
+      expect(errors).to include("Name can't be blank")
     end
     
     it 'raises an error if price is not provided' do
-      @category = Category.create!(name: 'Electronics')
       @product = Product.new(name: 'iPad', quantity: 32, category: @category)
 
       expect(@product.valid?).to be_falsey      
     end
     
     it 'raises an error if quantity is not provided' do
-      @category = Category.create!(name: 'Electronics')
       @product = Product.new(name: 'TV', price_cents: 330000000, category: @category)
 
       expect(@product.valid?).to be_falsey
