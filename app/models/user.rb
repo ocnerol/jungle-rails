54fbb39c0dbc6d0b8cc8684d23a52c970ede32e9
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-02-08 05:32:14 +0000

    Set class variable for minimum password length and add to password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ActiveRecord::Base
+  @@minimum_password_length = 8
 
   has_secure_password
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :password, presence: true
+  validates :password, presence: true, length: { minimum: @@minimum_password_length }
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   
 end
