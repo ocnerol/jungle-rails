9bffe1f8e9bc0a4919d77edff439b5fc7f4e802f
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-02-08 05:16:11 +0000

    Add validation to ensure a last name is present when creating a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
 
   has_secure_password
   validates :first_name, presence: true
+  validates :last_name, presence: true
   validates :password, presence: true
   validates :email, uniqueness: { case_sensitive: false }
   
