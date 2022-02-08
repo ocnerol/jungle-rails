cfe8922aedc415843da99787811bc9802132cd7d
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-02-08 02:46:04 +0000

    Add spec to test a product with the four required fields set will save successfully

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,6 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    # validation tests/examples go here
+
+    it 'successfully saves a product with all required fields present' do
+      @category = Category.create!(name: 'Electronics')
+      @product = Product.create!(name: 'iPhone', price_cents: 2000000, quantity: 4, category: @category)
+
+      # checks to see if product newly created was successfully saved in database
+      expect(Product.exists?(@product.id)).to be_truthy
+    end
+
   end
 end
