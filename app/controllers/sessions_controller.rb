2226e9c8085d4d14cbc97f71a624ec74b880cef2
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-02-08 18:03:32 +0000

    Remove commented copy of old version of create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,18 +13,6 @@ class SessionsController < ApplicationController
       redirect_to :login
       end
   end
-  # def create
-  #   user = User.find_by_email(params[:email])
-  #   #if the user exists AND the password entered is correct
-  #   if user && user.authenticate(params[:password])
-  #     # save the user id inside the browser cookie; this is how we keep the user
-  #     # logged in when they navigate around our website
-  #     session[:user_id] = user.id
-  #     redirect_to :root
-  #     else
-  #     redirect_to :login
-  #     end
-  # end
 
   def destroy
     session[:user_id] = nil
