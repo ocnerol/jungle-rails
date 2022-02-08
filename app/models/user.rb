ef608c2bfd71417ab076dd70de9b87f3e0084b74
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-02-08 05:13:54 +0000

    Add validation to ensure a first_name is present

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
 
   has_secure_password
-
+  validates :first_name, presence: true
   validates :password, presence: true
   validates :email, uniqueness: { case_sensitive: false }
   
