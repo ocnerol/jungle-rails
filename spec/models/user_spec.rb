8c1216285b9f89027220107c67a28e19f93a0d75
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-02-08 04:55:43 +0000

    Update test to use 'password instead of password_digest

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,11 +2,12 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'Validations' do
+    
     it 'successfully saves when provided matching password and password_confirmation' do
       user = User.create({
         first_name: 'Louis',
         last_name: 'Torres',
-        password_digest: '123pass',
+        password: '123pass',
         password_confirmation: '123pass',
         email: 'Louis@email.com'
           })
