99128049bc321a9bbb345a5fbf31461fbcccb407
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-02-08 04:49:52 +0000

    Add spec to ensure a user with required fields can be successfully saved to db

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    it 'successfully saves when provided matching password and password_confirmation' do
+      user = User.create({
+        first_name: 'Louis',
+        last_name: 'Torres',
+        password_digest: '123pass',
+        password_confirmation: '123pass',
+        email: 'Louis@email.com'
+          })
+
+      expect(User.exists?(user.id)).to be_truthy
+    end
+    
+  end
 end
