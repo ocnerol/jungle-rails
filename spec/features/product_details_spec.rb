99b26029ae3f2d7bdebc55df5b88580c2f6f88d0
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-02-09 01:52:07 +0000

    Generate category and 17 products for spec

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,4 +1,21 @@
 require 'rails_helper'
 
 RSpec.feature "Visitor navigates to product page from homepage by clicking a product", type: :feature, js: do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    17.times do |n|
+      @category.products.create!(
+        name: Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+
 end
