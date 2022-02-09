74b2453c10a3ebcd0c221a8a6e4843b089bb0bad
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-02-09 04:32:32 +0000

    select random product add button and store in variable

--- spec/features/add_to_cart_spec.rb
+++ spec/features/add_to_cart_spec.rb
@@ -15,4 +15,12 @@ RSpec.feature "Visitor can add products to their cart", type: :feature, js: true
         )
       end
     end
+
+    scenario "They click a product's 'Add' button and their cart in the navbar increases by 1" do
+      # ACT
+      visit root_path
+      random_product_add_button = page.all('button.add-product').sample
+
+      # DEBUG / VERIFY
+    end
 end
